208cfc94ae170aadf74bab46f8cddc5cbf7ffc9b
김현우 <hyun0310woo@gmail.com>
2020-12-03 00:39:47 +0900

    Fix SoreScreen.css

--- components/ScoreScreen.js
+++ components/ScoreScreen.js
@@ -1,17 +1,34 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { StyleSheet, Text, View, TouchableOpacity, TextInput } from 'react-native';
 
 export default function Select() {
+  const [point, setPoint] = useState(0);
+
+  
   return (
     <View style={styles.container}>
       <Text style={styles.startText}>목표 점수를 입력하세요.</Text>
-      <Text style={styles.user1}>1P</Text>
-      <TextInput style={styles.scoreInput}></TextInput>
-      <Text style={styles.user2}>2P</Text>
-      <TextInput style={styles.scoreInput}></TextInput>
+      <View style={styles.users}>
+        <View style={styles.user1Box}>
+          <Text style={styles.user1}>1P</Text>
+          <TextInput
+            style={styles.scoreInput}
+            onChangeText={text => onChangeText(text)}
+            value={point}>
+          </TextInput>
+        </View>
+        <View style={styles.user2Box}>
+          <Text style={styles.user2}>2P</Text>
+          <TextInput
+            style={styles.scoreInput}
+            onChangeText={text => onChangeText(text)}
+            value={point}>
+          </TextInput>
+        </View>
+      </View>
       <TouchableOpacity
         style={styles.selectBt}>
-        <Text style={styles.selectText}>확인</Text>
+      <Text style={styles.selectText}>확인</Text>
       </TouchableOpacity>
       <Text style={styles.endText}>점수를 1점 단위로 입력하세요.</Text>
     </View>
@@ -25,39 +42,57 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     width: "100%",
-
   },
 
   startText: {
       fontSize: 30,
       color: "#fff",
-      fontWeight: "bold"
+      fontWeight: "bold",
+  },
+
+  users: {
+    flexDirection: "row"
+  },
+
+  user1Box: {
+    flexDirection: 'column',
+    marginRight: 150,
+    alignItems: 'center',
+  },
+
+  user2Box: {
+    flexDirection: 'column',
+    marginLeft: 150,
+    alignItems: 'center',
   },
 
   user1: {
     color: "#FFD600",
-    fontSize: 30,
-    fontWeight: "bold",
+    fontSize: 40,
+    fontWeight: "bold"
   },
 
   user2: {
     color: "#fff",
-    fontSize: 30,
+    fontSize: 40,
     fontWeight: "bold",
+
   },
 
   scoreInput: {
     backgroundColor: "#fff",
-    width: 100,
-    height: 30,
+    width: 140,
+    height: 140,
     borderRadius: 8,
-    marginBottom: 10,
+    marginBottom: 20,
     textAlign: "center",
-    fontSize: 23
+    fontSize:50
   },
   
   selectText: {
-    fontWeight: "bold"
+    fontWeight: "bold",
+
+
   },
 
   selectBt: {
@@ -67,11 +102,12 @@ const styles = StyleSheet.create({
       height: 30,
       alignItems: 'center',
       justifyContent: 'center',
+      // marginTop: 30
   },
   
   endText: {
     color: "#fff",
-    marginTop: 20
+    marginTop: 10
   }
 
 
